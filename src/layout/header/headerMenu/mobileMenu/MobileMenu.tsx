4e771c0f07187e09074d33566ba51199337b68f1
vasilyocheck <vasily.medvedev1986@gmail.com>
2023-08-11 07:39:29 +0300

    add mob menu popup events

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Menu} from "../menu/Menu";
 import { S } from '../HeaderMenu_Styles'
 
 export const MobileMenu: React.FC<{menuItems: Array<string>}> = (props: {menuItems: Array<string>}) => {
+    const [menuIsOpen, setMenuIsOpen] = useState(false);
+    const onBurgerBtnClick = () => {
+        setMenuIsOpen(!menuIsOpen);
+    }
     return (
         <S.StyledMobileMenu>
-            <S.BurgerButton isOpen={false}>
+            <S.BurgerButton isOpen={menuIsOpen} onClick={onBurgerBtnClick}>
                 <span></span>
             </S.BurgerButton>
-            <S.MobileMenuPopup isOpen={false}>
+            <S.MobileMenuPopup isOpen={menuIsOpen} onClick={() => {setMenuIsOpen(false)}}>
                 <Menu menuItems={props.menuItems} />
             </S.MobileMenuPopup>
         </S.StyledMobileMenu>
