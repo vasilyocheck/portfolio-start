f67161f57dbe5acd8458ad14379a132085b35c58
vasilyocheck <vasily.medvedev1986@gmail.com>
2023-08-13 09:28:43 +0300

    add framer motion to my works section

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -6,9 +6,8 @@ import socialImg from '../../../assets/images/soc_networks.webp';
 import timerImg from '../../../assets/images/countdown.webp'
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Container} from "../../../components/Container";
-import { S } from './Works_Styles'
-
-/*const tabsItems = ['all', 'landing page', 'react', 'spa'];*/
+import { S } from './Works_Styles';
+import {AnimatePresence, motion} from "framer-motion"
 
 const tabsItems: Array<{ title: string, status: TabStatusType }> = [
     {
@@ -34,13 +33,43 @@ const worksData = [
         imgSrc: socialImg,
         title: 'Social Network',
         text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-        type: 'spa'
+        type: 'spa',
+        id: 1
     },
     {
         imgSrc: timerImg,
         title: 'Timer',
         text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim.',
-        type: 'react'
+        type: 'react',
+        id: 2
+    },
+    {
+        imgSrc: socialImg,
+        title: 'Social Network',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        type: 'spa',
+        id: 3
+    },
+    {
+        imgSrc: timerImg,
+        title: 'Timer',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim.',
+        type: 'react',
+        id: 4
+    },
+    {
+        imgSrc: socialImg,
+        title: 'Social Network',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        type: 'spa',
+        id: 5
+    },
+    {
+        imgSrc: timerImg,
+        title: 'Timer',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim.',
+        type: 'react',
+        id: 6
     }
 ]
 
@@ -71,15 +100,26 @@ export const Works: React.FC = () => {
                          currentFilterStatus={currentFilterStatus}
                 />
                 <FlexWrapper justify={'space-between'} align={'flex-start'} wrap={'wrap'}>
-                    {filteredWorks.map((work, ind) => {
-                        return (
-                            <Work imgSrc={work.imgSrc}
-                                  title={work.title}
-                                  text={work.text}
-                                  key={ind}
-                            />
-                        )
-                    })}
+                    <AnimatePresence>
+                        {filteredWorks.map((work) => {
+                            return (
+                                <motion.div style={{width: '400px', flexGrow: 1, maxWidth: '540px'}}
+                                    layout={true}
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    exit={{ opacity: 0 }}
+                                    key={work.id}
+                                >
+                                    <Work imgSrc={work.imgSrc}
+                                          title={work.title}
+                                          text={work.text}
+                                          key={work.id}
+                                    />
+                                </motion.div>
+
+                            )
+                        })}
+                    </AnimatePresence>
                 </FlexWrapper>
             </Container>
         </S.Works>
--- src/layout/sections/works/Works_Styles.ts
+++ src/layout/sections/works/Works_Styles.ts
@@ -12,17 +12,16 @@ const Works = styled.section`
 `;
 const Work = styled.div`
   background-color: ${theme.colors.secondaryBg};  
-  width: 330px;
-  flex-grow: 1;
+  
   ${Link} {
     padding: 10px 0;
     & + ${Link} {
       margin-left: 20px;
     }
   }
-  @media ${theme.media.desktop}{
+  /*@media ${theme.media.desktop}{
     max-width: 540px;
-  };
+  };*/
 `;
 
 const ImageWrapper = styled.div`
